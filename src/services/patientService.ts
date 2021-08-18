98f1cede2ecd31594733b8f3cbd64024ae27f5bd
Whishes <nicholas.cruickshank25@gmail.com>
2021-08-18 21:49:10 +1000

    maybe

--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -1,38 +1,48 @@
 import patientData from "../../data/patients";
-import { NonSensitivePatient, Patient, NewPatientEntry, Entry, newEntry} from "../types";
-import { v4 as uuidv4 } from 'uuid';
+import {
+  NonSensitivePatient,
+  Patient,
+  NewPatientEntry,
+  Entry,
+  newEntry,
+} from "../types";
+import { v4 as uuidv4 } from "uuid";
 
 const patients: Array<Patient> = patientData;
 
 const getEntries = (): Patient[] => {
-    return patients;
+  return patients;
 };
 
 const getNonSensitiveEntries = (): NonSensitivePatient[] => {
-    return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
-        id, name, dateOfBirth, gender, occupation
-    }));
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
 };
 
 const findById = (id: string): Patient | undefined => {
-    return patients.find(p => p.id === id);
-}
+  return patients.find((p) => p.id === id);
+};
 
 const addPatient = (patient: NewPatientEntry): Patient => {
-    const newPatientEntry: Patient = {
-        id: uuidv4(),
-        ...patient,
-        entries: []
-    };
+  const newPatientEntry: Patient = {
+    id: uuidv4(),
+    ...patient,
+    entries: [],
+  };
 
-    patients.push(newPatientEntry);
-    return newPatientEntry;
-}
+  patients.push(newPatientEntry);
+  return newPatientEntry;
+};
 
 const addEntry = (patientId: string, newEntry: newEntry): Entry => {
-    const id = uuidv4();
-    const entryWithID = { ...newEntry, id };
-    patients.forEach((patient) => {
+  const id = uuidv4();
+  const entryWithID = { ...newEntry, id };
+  patients.forEach((patient) => {
     if (patient.id === patientId) {
       patient.entries.push(entryWithID);
       return patient;
@@ -44,9 +54,9 @@ const addEntry = (patientId: string, newEntry: newEntry): Entry => {
 };
 
 export default {
-    getEntries,
-    getNonSensitiveEntries,
-    findById,
-    addPatient,
-    addEntry
+  getEntries,
+  getNonSensitiveEntries,
+  findById,
+  addPatient,
+  addEntry,
 };
